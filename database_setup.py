3bfe081c4850725625025bb5f4e88f16b40225f7
Dimitar <dimdimvas@yahoo.com>
2016-12-29 21:21:43 -0800

    Database set up.
    Database will have 3 tables - User, Recipe, and Ingredients.
    
    User: store information about the creator of a recipe. Includes name, id, email, and picture. The last two are optional.
    
    Recipe: store info about a recipe. A recipe consists of a name, description, ingredients list, and instructions.
    
    Ingredients: list of ingredients for a recipe.
    
    TODO: Implement functionality where users can like and leave comments on other users posts (Maybe too much - We'll see)

---
+++ database_setup.py
@@ -0,0 +1,63 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+from sqlalchemy.dialects.postgresql import *
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
+
+class Recipe(Base):
+    __tablename__ = 'recipe'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    description = Column(String(50000))
+    instructions = Column(String(50000))
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'id': self.id,
+            'description': self.description,
+            'ingredients': self.ingredients,
+            'instructions': self.instructions,
+        }
+
+
+class Ingredient(Base):
+    __tablename__ = 'ingredient'
+
+    id = Column(Integer, primary_key=True)
+    content = Column(String(5000))
+    recipe_id = Column(Integer, ForeignKey('recipe.id'))
+    recipe = relationship(Recipe)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'content': self.content,
+            'recipe': self.recipe,
+            'recipe_id': self.description,
+        }
+
+engine = create_engine('sqlite:///recipeslist.db')
+
+
+Base.metadata.create_all(engine)
